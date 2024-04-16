b0460c449b47da420d5ca79c2a3b57e1de69cba0
Russ Hyde <russ@jumpingrivers.com>
2024-04-16 11:01:53 +0100

    configure / run create_browser_data() in browser-data script

--- scripts/create_browser_data.R
+++ scripts/create_browser_data.R
@@ -11,13 +11,39 @@ renv::use(
   "mrc-ide/tfpscanner@7ee27416d69ec3eaf7e4158f38c31125b3c38c7d"
 )
 
-message("Hello world!")
+# User parameters
+# - The scanner environment file ("path/to/scanner_output/scanner-env-YYYY-MM-DD.rds")
+# - The output directory for the treeviews
+# - Any non-default arguments for the `treeview()` function
+env_file <- file.path()
+output_dir <- file.path()
+treeview_args <- list(
+  # branch_cols = c("logistic_growth_rate", "clock_outlier"),
+  # mutations = c("S:A222V", "S:Y145H", "N:Q9L", "S:E484K"),
+  # lineages = c("AY\\.9", "AY\\.43", "AY\\.4\\.2"),
+  # output_format = c("rds", "html"),
+  # dendrogram_colours = c("#2166ac", "#738fc0", "#afbad4", "#e8e8e8", "#e0a9a4", "#ce6964", "#b2182b"),
+  # heatmap_width = 0.075,
+  # heatmap_offset = 8,
+  # heatmap_lab_offset = -6,
+  # heatmap_fill = c(`FALSE` = "grey90", `TRUE` = "grey70")
+)
 
-for(pkg in c("ggplot2", "igraph", "ggiraph", "ggtree", "tfpscanner")) {
-  message(pkg, " version: ", packageVersion(pkg))
-}
+## ============================================================================================== ##
 
-tfpscanner::create_browser_data(
-  "Some environment",
-  output_dir = "Some directory"
+# Arguments for `create_browser_data`
+browser_args <- append(
+  list(
+    e0 = normalizePath(env_file),
+    output_dir = normalisePath(output_dir)
+  ),
+  treeview_args
+)
+
+stopifnot(file.exists(browser_args$e0))
+stopifnot(dir.exists(browser_args$output_dir))
+
+do.call(
+  tfpscanner::create_browser_data,
+  browser_args
 )
