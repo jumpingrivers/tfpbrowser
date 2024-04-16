4dfdd5603c8774487badae41916cdfdfbaec3fbd
Russ Hyde <russ@jumpingrivers.com>
2024-04-16 11:01:15 +0100

    update version of tfpscanner used in browser-data script

--- scripts/create_browser_data.R
+++ scripts/create_browser_data.R
@@ -1,9 +1,14 @@
 renv::use(
+  # The CRAN versions of these packages should match those used in the 'renv.lock' for tfpbrowser,
+  # to ensure that the figure formatting
   "ggplot2@3.4.0",
   "igraph@1.5.0",
   "ggiraph@0.8.4",
   "ggtree@3.8.2",
-  "mrc-ide/tfpscanner@ab29cb0e2126c8792fa471ae3200e12144eaa173"
+  # This is the HEAD of `jumpingrivers:draft-all-features-202403` as of 2024-04-15
+  # If the commit ID is absent, this will use the latest 'master' branch
+  # Including the commit ID allows us to use development-versions of the 'tfpscanner' package
+  "mrc-ide/tfpscanner@7ee27416d69ec3eaf7e4158f38c31125b3c38c7d"
 )
 
 message("Hello world!")
